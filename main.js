7768ff58b7047407dbe5fce1d71059b16da7eda9
Erlangga Aditia <142164074+erladitia14@users.noreply.github.com>
2023-10-12 16:48:43 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+// console.log("Hello World");
+// alert("Hello World");
+// prompt("Hello World");
+
+// var Promnet = "Halo saya sedang belajar java"
+// console.log(Promnet);
+// Promnet = "Saya belajar javascript"
+// console.log(Promnet);
+
+// console.log(Promnet);
+// var Promnet = "Saya belajar javascript"
+// console.log(Promnet);
+
+// let Promnet = "Halo saya sedang belajar java"
+// console.log(Promnet);
+// Promnet = "Saya belajar javascript"
+// console.log(Promnet);
+
+// const Promnet = "Halo saya sedang belajar java"
+// console.log(Promnet);
+// Promnet = "Saya belajar javascript"
+// console.log(Promnet);
+
+// let totalPoin = prompt("Masukan total poin anda");
+//         if(totalPoin >= 100){
+//                 document.write("Selamat anda mendapatkan grade A");
+//         }else{
+//                 document.write("Anda tidak mendapatkan grade A");
+//         }
+5
+// x=6
+// y=3
+
+// console.log(x<10&&y>1)
+// console.log(x<10&&y<1)
+// console.log(x==10||y==5)
+// console.log(x==6||y==5)
+// console.log(!(x==y))
+
+// document.write(x<10&&y>1)
+// document.write("<br>")
+// document.write(x<10&&y<1)
+// document.write("<br>")
+// document.write(x==10||y==5)
+// document.write("<br>")
+// document.write(x==6||y==5)
+// document.write("<br>")
+// document.write(!(x==y))
